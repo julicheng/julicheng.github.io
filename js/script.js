a253069786fb5aef3421e90231af0a1878c6f8ff
Juli Cheng <julicheng@julis-mbp.lan>
2019-09-27 22:00:06 +0100

    Implement owl carousel

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    responsiveClass: true,
+    items: 3,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: false
+      }
+    }
+  });
+});
